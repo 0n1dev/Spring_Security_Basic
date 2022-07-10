6769fd31d172e56d7ded837635f7ac0be60f2396
0n1dev <all_mind@naver.com>
2022-07-10 17:23:00 +0900

    Project init

---
+++ src/main/kotlin/com/example/spring_security_basic/SpringSecurityBasicApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_security_basic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityBasicApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringSecurityBasicApplication>(*args)
+}
